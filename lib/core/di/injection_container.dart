4e5c6f5a9155c5f45ba85617ff0859f8a74af3ae
Pham Khanh Han <phamkhanhhan6@gmail.com>
2026-06-20 16:14:10 +0700

    feat(auth): implement session management and logout functionality

--- lib/core/di/injection_container.dart
+++ lib/core/di/injection_container.dart
@@ -105,6 +105,8 @@ Future<void> initDependencies() async {
   sl.registerLazySingleton(() => UpdatePasswordUseCase(sl()));
   sl.registerLazySingleton(() => ChangePasswordUseCase(sl()));
   sl.registerLazySingleton(() => LoginWithGoogleUseCase(sl()));
+  sl.registerLazySingleton(() => CheckSessionUseCase(sl()));
+  sl.registerLazySingleton(() => LogoutUseCase(sl()));
 
   // Đăng ký Cubit
   sl.registerFactory(
@@ -115,6 +117,8 @@ Future<void> initDependencies() async {
       updatePasswordUseCase: sl(),
       changePasswordUseCase: sl(),
       loginWithGoogleUseCase: sl(),
+      checkSessionUseCase: sl(),
+      logoutUseCase: sl(),
     ),
   );
 
